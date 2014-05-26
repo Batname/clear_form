ed842992a37f403a9825f7f1a4193c83d4c5a387
Batname <dadubinin@gmail.com>
2014-05-26 13:01:53 +0300

    ajax in byttons

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,21 +1,50 @@
-//$(document).ajaxStart(function () {
-//    if ($( "label" ).hasClass( "error" )) {
-//        $( ".error" ).hide();
-//        console.log("test");
-//    }
-//});
-//
-//
-$(document).ajaxSuccess(function () {
-    if ($(".error").is(":visible")) {
-        console.log("error show.");
-    } else {
-        $('#new_upload').on('click', '.btn', function () {
-            $("#battest").removeClass("input_upload input_upload_bg").addClass("input_upload loading_but");
-        })
-        console.log("error display none.");
-    }
+$(document).ajaxStart(function () {
+    $('#file_send').bind("click", function () {
+        var imgVal = $('#file_upload').val();
+        if (imgVal == '') {
+                if ($('#upload_name').val().length > 0) {
+                    if ($('#upload_email').val().length > 0) {
+                        if ($('#upload_massage').val().length > 0) {
+                            if ($(".error").is(":visible") == false) {
+                                $('#new_upload').on('click', '.btn', function () {
+                                    $("#battest").removeClass("input_upload input_upload_bg").addClass("input_upload loading_but");
+                                })
+                                console.log("error display none");
+                            }
+                        }
+                    }
+                }
+        } else {
+            if ($('#upload_name').val().length > 0) {
+                if ($('#upload_email').val().length > 0) {
+                    if ($('#upload_massage').val().length > 0) {
+                        console.log("error show.");
+                        $('#new_upload').on('click', '.btn', function () {
+                            $("#battest").removeClass("input_upload input_upload_bg").addClass("input_upload loading_but");
+                        })
+                    }
+                }
+            }
+        }
 
+    });
 });
+
+
+
+
+
+//
+//$(document).ajaxSuccess(function () {
+//    if ($(".error").is(":visible") == true) {
+//        console.log("error show.");
+//    } else {
+//        $('#new_upload').on('click', '.btn', function () {
+//            $("#battest").removeClass("input_upload input_upload_bg").addClass("input_upload loading_but");
+//        })
+//        console.log("error display none.");
+//    }
+//
+//});
 
 
